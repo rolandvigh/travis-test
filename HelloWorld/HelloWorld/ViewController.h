ffa55dbea779b6d3f17b69171ccbb7725b1aa976
Roland Vigh <roland.vigh@mattakis.com>
2016-03-22 12:10:30 +0100

    Initial commit

---
+++ HelloWorld/HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Roland Vigh on 3/22/16.
+//  Copyright © 2016 Roland Vigh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
